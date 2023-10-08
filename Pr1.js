26e109c77d67c7a5680d5e9b4ee5cd42fe51e2f5
choeseonmin <albamm520@naver.com>
2023-10-08 23:14:18 +0900

    변수 선언, 문자열 마무리

--- Pr1.js
+++ Pr1.js
@@ -48,6 +48,17 @@ console.log(typeof null)
 console.log(typeof undefined)
 
 
+const name = "MIKE";
+const age = 27;
+
+const a = "나는 ";
+const b = " 입니다.";
+
+console.log(a + name  + b);
+
+console.log(a + age + "살" + b);
+
+
 
 
 
---
+++ Pr2.js
@@ -0,0 +1,9 @@
+const name = "MIKE";
+const age = 27;
+
+const a = "나는 ";
+const b = " 입니다.";
+
+console.log(a + name  + b);
+
+console.log(a + age + "살" + b);
