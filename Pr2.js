52e7d2be0ab89cb93606fbb9ac968e4806927321
choeseonmin <albamm520@naver.com>
2023-10-09 01:44:06 +0900

    alert, prompt, confirm 이해

--- Pr2.js
+++ Pr2.js
@@ -1,9 +1,12 @@
-const name = "MIKE";
-const age = 27;
+//대화 상자
 
-const a = "나는 ";
-const b = " 입니다.";
+//alert 알림창, prompt 입력값을 받는 창을 만드는 함수?
 
-console.log(a + name  + b);
 
-console.log(a + age + "살" + b);
+const name = prompt("이름을 입력하세요.", "최선민"); //인풋창 placegholder 처럼 사용할 수도 있다.
+alert("환영합니다. "+ name + "님");
+alert("환영합니다. "+ name + "님");
+
+const isAdult = confirm("당신은 성인 입니까?") // alert는 알림창이기 때문에 확인 버튼만 있지만, confirm은 취소, 확인 버튼이 생김
+console.log(isAdult);
+
